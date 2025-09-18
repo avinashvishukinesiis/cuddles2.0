66102bf6540d312cd514dbe454527462f0838f7c
avinashvishukinesiis <avinash@kinesiis.com>
2025-09-19 01:50:58 +0530

    curriculum update

--- app/Curriculum/page.tsx
+++ app/Curriculum/page.tsx
@@ -1,6 +1,7 @@
 import DayAtCuddle from "@/components/Curriculum/DayAtCuddles/DayAtCuddles"
 import HeroSection from "@/components/Curriculum/HeroSection/HeroSection"
 import LetsConnect from "@/components/LetsConnect/letsConnect"
+import Testimonial from "@/components/Testimonial/Testimonial"
 
 
 const Curriculum = () => {
@@ -8,6 +9,7 @@ const Curriculum = () => {
      <div className="flex flex-col font-ohno">
       <HeroSection/>
       <DayAtCuddle/>
+      <Testimonial/>
       <LetsConnect/>
      </div>
   )
