a031d5623394015e88b7e566ff322a3df9938e55
avinashvishukinesiis <avinash@kinesiis.com>
2025-10-03 12:51:56 +0530

    sanity deploy done again

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -7,4 +7,11 @@ import { defineCliConfig } from 'sanity/cli'
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig({
+    deployment: {
+        appId: 'zi34c1qh43ruf4u58vzrshmy',
+    }, 
+    api: { 
+        projectId, dataset 
+    }
+})
